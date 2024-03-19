81bc03152a957ea9d9ab4c509e4b74504aa53097
David Paquin <djpaquin1@gmail.com>
2024-03-19 13:17:02 -0400

    curated list to remove special pages

--- generate_terminals.py
+++ generate_terminals.py
@@ -1,8 +1,5 @@
 from collections import defaultdict
 import requests
-from heapq import nlargest
-
-ranking = defaultdict(int)
 
 
 headers = {
@@ -14,7 +11,7 @@ headers = {
 }
 
 
-def process(year, month):
+def process(year, month, ranking):
     url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia.org/all-access/{year}/{month:02d}/all-days"
     resp = requests.get(url=url, headers=headers, timeout=10)
     resp.raise_for_status()
@@ -24,18 +21,35 @@ def process(year, month):
 
 
 def scroll_history():
+    ranking = defaultdict(int)
     for year in range(2016, 2024):
         for month in range(1, 13):
             print(f"Processing {year}-{month:02d}...", end="")
-            process(year, month)
+            process(year, month, ranking)
             print("done!")
+    return ranking
+
+
+def remove_specials(title):
+    return not (
+        title.split(":")[0]
+        in ("Special", "Help", "Wikipedia", "File", "User", "Template", "Category")
+        or title in ("Main_Page", "404.php")
+        or "disambiguation" in title.lower()
+    )
+
+
+def curate_terminal_articles():
+    ranking = scroll_history()
+    ranked = sorted(ranking, key=ranking.get, reverse=True)
+    curated = filter(remove_specials, ranked)
+    final = map(lambda s: s.replace("_", " "), curated)
+
+    with open("terminal_articles.txt", "w") as f:
+        for i, x in enumerate(final, 1):
+            # print(f"{i:2d}. {x:50s}", ranking[x])
+            print(x, file=f)
 
 
 if __name__ == "__main__":
-    scroll_history()
-    biggest = sorted(ranking, key=ranking.get, reverse=True)
-    with open("terminal_articles.txt", "w") as f:
-        for i, x in enumerate(biggest, 1):
-            # print(f"{i:2d}. {x:50s}", ranking[x])
-            print(x.replace("_", " "), file=f)
-
+    curate_terminal_articles()
